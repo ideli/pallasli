2261e39565f03ce8c644b45d1f67e627cd577313
pallasLi <ytli1987@163.com>
2016-02-04 09:23:39 +0800

    1

---
+++ pallasli-root/pallasli-study-root/pallasli-study-mvn-jasmine/src/main/webapp/javascript/example.js
@@ -0,0 +1,8 @@
+// example.js
+define(function() {
+  return {
+    hello : function(name) {
+      return "hello " + name;
+    }
+  };
+});
---
+++ pallasli-root/pallasli-study-root/pallasli-study-mvn-jasmine/src/test/javascript/example.spec.js
@@ -0,0 +1,8 @@
+// example.spec.js
+//define("example", function(example) {
+//  describe("example.hello", function() {
+//    it("should return 'hello jim'", function() {
+//      expect(example.hello("jim")).toBe("hello jim");
+//    });
+//  });
+//});
